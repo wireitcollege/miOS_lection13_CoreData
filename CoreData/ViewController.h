c3c60457db69d05bdaff39342606b2c44bd99d8b
Rostyslav <kobizsky@gmail.com>
2015-08-11 09:28:25 +0300

    Initial Commit

---
+++ CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by Rostyslav Kobizsky on 8/11/15.
+//  Copyright (c) 2015 Rostyslav Kobizsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
